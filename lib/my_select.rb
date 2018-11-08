0ba9493acef95d533912a04bf8c781c6643a3c19
Molly McCarron <mccarronmollye@gmail.com>
2018-11-08 17:18:09 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+  i = 0
+ new_collection = []
+ while i < collection.length
+   new_collection << yield(collection[i])
+   i = i + 1
+ end
+ new_collection# your code here!
 end
